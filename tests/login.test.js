0d932dd1458fe1b93d639ce95188cb6b0ff6c15d
Mikhail Ming <amingaraev@smartdatatech.io>
2024-10-09 18:24:10 +0200

    fix tests titles

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,4 +1,4 @@
-import { test, expect, request } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import fs from 'fs';
 import path from 'path';
 import {login} from "../src/helpers";
@@ -20,39 +20,38 @@ test.describe('Landing Tests', () => {
         if (fs.existsSync(stateFilePath)) {
             // Если файл состояния существует, используем его
             const storageState = JSON.parse(fs.readFileSync(stateFilePath, 'utf-8'));
-            await page.context().addCookies(storageState.cookies);
         } else {
             // Если состояния нет, выполняем логин
             await loginAndSaveState(page);
         }
     });
 
-    test('Тест 1: Проверка токена и контента', async ({ page }) => {
+    test('0. Тест текста на главной странице', async ({ page }) => {
         await page.goto('https://staging.fanfrick.com/');
         const title = page.locator('.image-block__main-info >> p');
         await expect(title).toHaveText('Encourage users to interact with your social network accounts to access special rewards. Build your audience on Facebook, Instagram, Twitter, YouTube, TikTok and Linkedin.');
     });
 
-    test('Тест 2', async ({ page }) => {
+    test('1. Тест текста в кнопке', async ({ page }) => {
         await page.goto('https://staging.fanfrick.com/');
 
         const button = page.locator('.image-block__main-info >> button');
         await expect(button).toHaveText('My campaigns');
     });
 
-    test('Тест 3', async ({ page }) => {
-        await page.goto('https://staging.fanfrick.com/my-gates');та
+    test('2. Тест текста в заголовке на странице кампаний', async ({ page }) => {
+        await page.goto('https://staging.fanfrick.com/my-gates');
         const header = page.locator('h2');
         await expect(header).toHaveText('Campaigns');
     });
 
-    test('Тест 4', async ({ page }) => {
+    test('3. Тест текста в первом блоке на главной', async ({ page }) => {
         await page.goto('https://staging.fanfrick.com');
         const title = page.locator('.grow-block__content >> .title >> h2');
         await expect(title).toHaveText('Use Fanfrick to give your existing and new audience incentives and gifts for real action on your social channel. Increase your audience size and loyalties');
     });
 
-    test('Тест 5', async ({ page }) => {
+    test('4. Тест перехода на страницу How it works', async ({ page }) => {
         await page.goto('https://staging.fanfrick.com');
         const button = page.locator('.grow-block__content >> .grow-block-button');
         await expect(button).toHaveText('How it works');
